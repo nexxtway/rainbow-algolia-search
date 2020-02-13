a507fc2b2fc00b3bc29eb9ac75b1d8592cc985a1
Saul <oliverasaul9201@gmail.com>
2020-02-13 13:11:07 -0600

    chore: added spesific site to deploy

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Hello React</p>
+                <p>Wellcome to argolia books search</p>
             </header>
         </div>
     );

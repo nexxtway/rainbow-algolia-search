b03682c1a0028affb5ffb8f550aa8efaca164cce
Saul <oliverasaul9201@gmail.com>
2020-02-14 00:39:41 -0600

    chore: added rainbow libs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FirebaseProvider } from 'rainbow-firebase-hooks';
+import { Application } from 'react-rainbow-components';
+import firebaseApp from './firebase';
+
+const value = { app: firebaseApp };
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello React</p>
-                <p>Wellcome to algolia books search</p>
-            </header>
-        </div>
+        <FirebaseProvider value={value}>
+            <Application>{/* Routes */}</Application>
+        </FirebaseProvider>
     );
 }
 

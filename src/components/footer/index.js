c5a7eebdf5705abf11af59141ea63511868602ac
José Faro <blakmetall@gmail.com>
2020-02-17 16:31:58 -0600

    feat: initialize layout ui

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Styled = styled.div`
-    width: 100%;
-    background: red;
-    color: white;
+const FooterWrapper = styled.div`
+    position: fixed;
+    left: 0%;
+    top: auto;
+    right: 0%;
+    bottom: 0%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
+const FooterContainer = styled.div`
+    display: flex;
+    width: 100%;
+    max-width: 1393px;
+    padding-right: 20px;
+    padding-left: 20px;
+    justify-content: space-between;
+`;
+
+const FooterCopyright = styled.div`
+    width: 100%;
+    color: #576574;
+    font-size: 16px;
+    text-align: center;
+`;
+
+const year = new Date().getFullYear();
+
 const Footer = () => {
-    return <Styled>Footer</Styled>;
+    return (
+        <FooterWrapper>
+            <FooterContainer>
+                <FooterCopyright>© Copyright Nexxtway {year}</FooterCopyright>
+            </FooterContainer>
+        </FooterWrapper>
+    );
 };
 
 export default Footer;
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -6,19 +6,31 @@ import Footer from '../footer';
 
 const MainWrapper = styled.main`
     width: 100%;
+    height: 100vh;
+    padding-bottom: 43px;
+    background-color: #f4f6f9;
+    font-family: Montserrat, sans-serif;
+    color: #a4a7b5;
+    font-size: 24px;
+    line-height: 1.4em;
+    font-weight: 400;
 `;
 
-const InnerWrapper = styled.main`
-    height: 600px;
-    background: gray;
-    color: white;
+const Container = styled.main`
+    width: 100%;
+    max-width: 1030px;
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 20px;
+    padding-left: 20px;
+    border: 1px solid red;
 `;
 
 const Layout = ({ children }) => (
     <>
         <MainWrapper>
             <Header />
-            <InnerWrapper>{children}</InnerWrapper>
+            <Container>{children}</Container>
             <Footer />
         </MainWrapper>
     </>

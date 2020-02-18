82be5f14def4c32023bb11a0dcf4025ea5690a0e
Saul <oliverasaul9201@gmail.com>
2020-02-18 10:46:43 -0600

    fix: cloud functions to no saving unnecessary attributes in algolia

--- functions/src/firestore/createBookInAlgolia/index.js
+++ functions/src/firestore/createBookInAlgolia/index.js
@@ -2,5 +2,17 @@ const functions = require('firebase-functions');
 const { writeDocumentToAlgolia } = require('../../../libs/algolia');
 
 module.exports = functions.firestore.document('books/{uid}').onCreate((snapshot, context) => {
-    return writeDocumentToAlgolia(snapshot.id, snapshot.data());
+    const { title, authors, cover, averageRating, releaseDate, isbn, language } = snapshot.data();
+
+    const recordData = {
+        title,
+        authors,
+        cover,
+        averageRating,
+        releaseDate,
+        isbn,
+        language,
+    };
+
+    return writeDocumentToAlgolia(snapshot.id, recordData);
 });

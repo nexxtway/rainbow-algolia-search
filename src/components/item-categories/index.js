f68a32bc44fcbe96039a2abf99b977a078a9d539
José Faro <blakmetall@gmail.com>
2020-02-21 11:49:26 -0600

    fix: component improvement
    * back component
    * item-categories component

--- src/components/item-categories/index.js
+++ src/components/item-categories/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import RenderIf from 'react-rainbow-components/components/RenderIf';
 import PropTypes from 'prop-types';
 import { Badge } from 'react-rainbow-components';
 import { Wrapper, Label } from './styled';
 
 const ItemCategories = props => {
-    const { className, style, categories } = props;
+    const { hideLabel, className, style, categories } = props;
 
     const hasCategories = !!categories.length;
 
     if (hasCategories) {
         return (
             <Wrapper className={className} style={style}>
-                <Label>Categories</Label>
+                <RenderIf isTrue={!hideLabel}>
+                    <Label>Categories</Label>
+                </RenderIf>
 
                 {categories.map((category, index) => {
                     const key = `${category}-${index}`;
@@ -27,11 +30,13 @@ const ItemCategories = props => {
 export default ItemCategories;
 
 ItemCategories.propTypes = {
+    hideLabel: PropTypes.bool,
     className: PropTypes.string,
     style: PropTypes.object,
     categories: PropTypes.array,
 };
 ItemCategories.defaultProps = {
+    hideLabel: false,
     className: undefined,
     style: undefined,
     categories: [],
